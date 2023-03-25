adbad13ec63a1b115e065f79415e9782d6b060d6
IlyTom <126102373+IlyTom@users.noreply.github.com>
2023-03-25 15:17:23 +0300

    2 задача

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,23 +1,25 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-string[] InputMassive()
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] InputConst()
 {
-    System.Console.WriteLine("Введите числа через запятую с пробелами:");
-    string[] arr = Console.ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+    System.Console.WriteLine("Введите коэффициенты прямой k и b:");
+    double[] arr = new double[2];
+    arr[0] = double.Parse(Console.ReadLine()!);
+    arr[1] = double.Parse(Console.ReadLine()!);
     return arr;
 }
 
-int CountNum(string[] arr)
+void CheckCoordinateXLine(double[] arr1, double[] arr2)
 {
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (int.Parse(arr[i]) > 0) count++;
-    }
-    return count;
+    double x = (arr2[1] - arr1[1]) / (arr1[0] - arr2[0]);
+    double y = arr1[0] * x + arr1[1];
+    System.Console.WriteLine($"Точка пересечения двух прямых находиться в точке ({x}; {y})");
 }
 
-string[] Array = InputMassive();
-System.Console.WriteLine($"Количетво чисел больше 0 в массиве равно {CountNum(Array)}");
+double[] Array1 = InputConst();
+double[] Array2 = InputConst();
+
+CheckCoordinateXLine(Array1, Array2);
