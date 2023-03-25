aa9e0da19a6784dc40486ad199282cc4941a804a
IlyTom <126102373+IlyTom@users.noreply.github.com>
2023-03-25 15:04:06 +0300

    1 задача

---
+++ HomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string[] InputMassive()
+{
+    System.Console.WriteLine("Введите числа через запятую с пробелами:");
+    string[] arr = Console.ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+    return arr;
+}
+
+int CountNum(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (int.Parse(arr[i]) > 0) count++;
+    }
+    return count;
+}
+
+string[] Array = InputMassive();
+System.Console.WriteLine($"Количетво чисел больше 0 в массиве равно {CountNum(Array)}");
